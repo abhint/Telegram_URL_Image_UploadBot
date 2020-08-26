54d67c532bb9dfe303c558bbc89b105b5de5c5f0
Abhijith NT <abhijithnt64@gmail.com>
2020-08-26 23:38:29 +0530

    Fixed

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-class my():
+class config():
     BOT_TOKEN = os.environ["BOT_TOKEN"]    # from @botfather
     API_ID = int(os.environ["API_ID"])     # from https://my.telegram.org/apps
     API_HASH = os.environ["API_HASH"]      # from https://my.telegram.org/apps

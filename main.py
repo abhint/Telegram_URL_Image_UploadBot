16c6f1d254e52e89e38cbbdab84bb2642fa9b080
Abhijith NT <abhijithnt64@gmail.com>
2020-08-26 23:37:32 +0530

    Fixed

--- main.py
+++ main.py
@@ -5,32 +5,15 @@
 # Thank you https://github.com/pyrogram/pyrogram :-)
 
 from pyrogram import Client
-from config import my
+from config import config
 plugins = dict(
         root="plugins"
     )
 bot = Client(
     "Image upload bot",
-    bot_token = my.BOT_TOKEN,
-    api_id = my.API_ID,
-    api_hash = my.API_HASH,
+    bot_token = config.BOT_TOKEN,
+    api_id = config.API_ID,
+    api_hash = config.API_HASH,
     plugins = plugins
 )
 bot.run()
-
-
-
-
-# from pyrogram import Client
-# plugins = dict(
-#         root="plugins"
-#     )
-# bot = Client(
-#     "Image Uploading Bot",
-#     bot_token = '1306098991:AAGgo3F2AoTCrpYtU1gaAyvI2J7nxcqvkwg',
-#     api_id = 1427768,
-#     api_hash = 'be224f7cf30ad49c481198d7fb010983',
-#     plugins = plugins
-# )
-
-# bot.run()

8a233e105f86b879eda560ebe0d75986581ea83c
Abhijith N T <abhijithnt64@gmail.com>
2021-06-18 21:35:08 +0530

    Update bot.py

--- plugins/bot.py
+++ plugins/bot.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-# This is bot coded by Abhijith-cloud and used for educational purposes only
-# https://github.com/Abhijith-cloud
-# (c) Abhijith N T ;-)
-# Thank you https://github.com/pyrogram/pyrogram :-)
+# This is bot coded by Abhijith N T and used for educational purposes only
+# https://github.com/AbhijithNT
+# (c) Abhijith N T
+# Thank you https://github.com/pyrogram/pyrogram 
 
 
 import os
